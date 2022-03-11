8733f1d48a694322d27f2c394577ad5f2233a01c
Gayathry.Ekarath <gayathry.ekarath@ust.com>
2022-03-11 13:18:49 +0530

    Commit

--- src/main/java/com/example/demo/FormController.java
+++ src/main/java/com/example/demo/FormController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import java.util.Optional;
 import java.util.List;
+
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -16,8 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 @Controller
 public class FormController {
 	
@@ -26,63 +30,82 @@ public class FormController {
 	@RequestMapping("/")
 	public String details()
 	{
+	log.info("details method started");	
 	return "student";
+	
 	}
+	
 	@RequestMapping("/details")
 	public String details(Students students)
 	{
+	log.info("details method started with passing parameter students{}",students);		
     repo.save(students);
+    log.info("details method ended");
 	return "student";
 	}
 	
 	@RequestMapping("/getdetails")
 	public String getdetails()
 	{
+	log.info("getDetails method started");	
+	log.info("getDetails method ended");
     return "ViewStudents";
 	}	
 	
 @PostMapping("/getdetails")
 public ModelAndView getdetails(@RequestParam int sid) 
 {
+	log.info("getdetails method started with passing parameter student Id{}",sid);
 	ModelAndView mv=new ModelAndView("Retrieve");
 	Students students=repo.findById(sid).orElse(null);
     mv.addObject(students);
+    log.info("getDetails by id method ended");
     return mv;
 } 
 @RequestMapping("/students")
 @ResponseBody
 public List<Students> getStudents()
 {
+	log.info("getStudents method started");	
+	log.info("getStudents method ended");
 	return repo.findAll();
 }
 
 @RequestMapping("/students/{sid}")
 @ResponseBody
-public Optional<Students> getStudents2(@PathVariable("sid") int sid)
+public Optional<Students> getStudentById(@PathVariable("sid") int sid)
 {
+
+	log.info("getStudentById method started by passing pathvariable{}",sid);	
+	log.info("getStudentById method ended");
 	return repo.findById(sid);
 }
 
 @PostMapping("/students")
-public Students getStudents3(@RequestBody Students students)
+public Students saveStudents(@RequestBody Students students)
 {
+	log.info("saveStudents method started with passing parameter students{}",students);
 	repo.save(students);
+	log.info("saveStudents method ended");
 	return students;
 }
 
 @DeleteMapping("/students/{sid}")
-public Students getStudents4(@PathVariable("sid") int sid)
+public Students deleteStudents(@PathVariable("sid") int sid)
 {   
+	log.info("deleteStudents method started by passing pathvariable{}",sid);	
 	Students std=repo.getOne(sid);
 	repo.delete(std);
+	log.info("deleteStudents method ended");
 	return std;
 }
 
 @PutMapping(path="/students", consumes= {"application/json"})
-public Students getStudents5(@RequestBody Students students)
+public Students updateStudents(@RequestBody Students students)
 {   
-	
+	log.info("updateStudents method started with passing parameter students{}",students);
 	repo.save(students);
+	log.info("updateStudents method ended");
 	return students;
 }
 
--- src/main/java/com/example/demo/Students.java
+++ src/main/java/com/example/demo/Students.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@ToString
 @Entity
 public class Students {
 	@Id
@@ -10,37 +16,5 @@ public class Students {
 	private String sname;
 	private String stopic;
 	private int smark;
-	
-	
-	public int getSid() {
-		return sid;
-	}
-	public void setSid(int sid) {
-		this.sid = sid;
-	}
-	public String getSname() {
-		return sname;
-	}
-	public void setSname(String sname) {
-		this.sname = sname;
-	}
-	public String getStopic() {
-		return stopic;
-	}
-	public void setStopic(String stopic) {
-		this.stopic = stopic;
-	}
-	public int getSmark() {
-		return smark;
-	}
-	public void setSmark(int smark) {
-		this.smark = smark;
-	}
-	
-	@Override
-	public String toString() {
-		return "Students [sid=" + sid + ", sname=" + sname + ", stopic=" + stopic + ", smark=" + smark + "]";
-	}
-	
 
 }
